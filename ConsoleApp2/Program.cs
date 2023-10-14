46dacbc4f5cad45b80313b4ac22b4c3a56615732
hamle <hamle@DESKTOP-LCJ4CGJ>
2023-10-14 11:36:41 -0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//Escribir un programa en el cual se ingresen cuatro números,
+//calcular e informar la suma de los dos primeros yel producto del tercero y
+//el cuarto
+
+using ConsoleApp2.ejercicio2;
+
+Aritmetica c;
+Console.WriteLine(" Ingrese el primer numero ");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine(" Ingrese el segundo numero ");
+double num2 = double.Parse(Console.ReadLine());
+Console.WriteLine(" Ingrese el tercer numero ");
+double num3 = double.Parse(Console.ReadLine());
+Console.WriteLine(" Ingrese el cuarto numero ");
+double num4 = double.Parse(Console.ReadLine());
+
+c = new Aritmetica(num1, num2, num3, num4);
+
+Console.WriteLine(" La suma de los primeros numeros es:" + c.Getsuma() + " EL producto de los dos ultimos numeros es: " + c.Producto());
---
+++ ConsoleApp2/ejercicio2/Aritmetica.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp2.ejercicio2
+{
+    internal class Aritmetica
+    {
+        private double num1;
+        private double num2;
+        private double num3;
+        private double num4;
+
+        public Aritmetica (double num1, double num2, double num3, double num4)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.num3 = num3;
+            this.num4 = num4;
+        }
+
+        public double Getsuma()
+        {
+            return num1 + num2;
+        }
+
+        public double Producto()
+        {
+            return num3 * num4;
+        }
+    }
+}
